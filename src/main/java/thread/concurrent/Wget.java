b8a021a35321e7acc582cdd7c12d61d4d1847a2a
PavelChupin <pavelchupin94@yandex.ru>
2023-11-09 13:32:34 +0300

    4. Режим ожидания. Фикс

--- src/main/java/thread/concurrent/Wget.java
+++ src/main/java/thread/concurrent/Wget.java
@@ -6,7 +6,7 @@ public class Wget {
                 () -> {
                     try {
                         System.out.println("Start loading ... ");
-                        for (int i = 0; i < 100; i++) {
+                        for (int i = 0; i <= 100; i++) {
                             Thread.sleep(1000);
                             System.out.print("\rLoading : " + i + "%");
                         }
