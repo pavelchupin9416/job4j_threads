6113f57a1187f6e5e767133a8a90bd844133232c
PavelChupin <pavelchupin94@yandex.ru>
2023-11-13 17:26:15 +0300

    2. Модель памяти Java [#267917]

---
+++ src/main/java/thread/sharedresources/DCLSingleton.java
@@ -0,0 +1,21 @@
+package thread.sharedresources;
+
+public final class DCLSingleton {
+
+    private static volatile  DCLSingleton inst;
+
+    public static DCLSingleton instOf() {
+        if (inst == null) {
+            synchronized (DCLSingleton.class) {
+                if (inst == null) {
+                    inst = new DCLSingleton();
+                }
+            }
+        }
+        return inst;
+    }
+
+    private DCLSingleton() {
+    }
+
+}
