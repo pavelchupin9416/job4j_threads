3e9319f3d16366e2015ca4b737dff3dbacb9ec86
PavelChupin <pavelchupin94@yandex.ru>
2023-11-13 15:54:21 +0300

    1. Синхронизация общих ресурсов. [#1096]

---
+++ src/main/java/thread/sharedresources/Cache.java
@@ -0,0 +1,12 @@
+package thread.sharedresources;
+
+public final class Cache {
+    private static Cache cache;
+
+    public static synchronized Cache instOf() {
+        if (cache == null) {
+            cache = new Cache();
+        }
+        return cache;
+    }
+}
