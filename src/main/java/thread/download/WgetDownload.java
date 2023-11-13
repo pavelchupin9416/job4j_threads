3c1ef9481a2ad62b27cf7b0a66faa339fe52a6ac
PavelChupin <pavelchupin94@yandex.ru>
2023-11-13 12:35:48 +0300

    8. Скачивание файла с ограничением. Фикс.

--- src/main/java/thread/download/WgetDownload.java
+++ src/main/java/thread/download/WgetDownload.java
@@ -1,6 +1,5 @@
 package thread.download;
 
-import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -9,30 +8,36 @@ import java.net.URL;
 public class WgetDownload implements Runnable {
     private final String url;
     private final int speed;
+    private final String name;
 
-    public WgetDownload(String url, int speed) {
+    public WgetDownload(String url, int speed, String name) {
         this.url = url;
         this.speed = speed;
+        this.name = name;
     }
 
     @Override
     public void run() {
-        var startAt = System.currentTimeMillis();
-        var file = new File("tmp2.xml");
+        var file = new File(name);
+        System.out.println(name);
         try (var in = new URL(url).openStream();
              var out = new FileOutputStream(file)) {
-            System.out.println("Open connection: " + (System.currentTimeMillis() - startAt) + " ms");
+
             var dataBuffer = new byte[512];
             int bytesRead;
+            int totalBytes = 0;
+            var startAt = System.currentTimeMillis();
             while ((bytesRead = in.read(dataBuffer, 0, dataBuffer.length)) != -1) {
-                var downloadAt = System.nanoTime();
                 out.write(dataBuffer, 0, bytesRead);
-                double nano = (System.nanoTime() - downloadAt);
-                System.out.println("Read 512 bytes : " + nano + " nano.");
-                double sleep = 512 / nano * 1000000 / speed;
-                if (sleep > 1) {
-                System.out.println("sleep " + sleep);
-                Thread.sleep((int) sleep);
+                totalBytes += bytesRead;
+                if (totalBytes >= speed) {
+                    long pause = 1000 - (System.currentTimeMillis() - startAt);
+                    if (pause > 0) {
+                        Thread.sleep(pause);
+                        System.out.println("sleep " + pause);
+                    }
+                    totalBytes = 0;
+                    startAt = System.currentTimeMillis();
                 }
             }
         } catch (IOException e) {
@@ -51,8 +56,10 @@ public class WgetDownload implements Runnable {
         }
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
-        Thread wget = new Thread(new WgetDownload(url, speed));
+        String name = args[2];
+        Thread wget = new Thread(new WgetDownload(url, speed, name));
         wget.start();
         wget.join();
     }
 }
+
