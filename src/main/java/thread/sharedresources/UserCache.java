c820b7823c933e92b00d8c25f635382fa89edeb6
PavelChupin <pavelchupin94@yandex.ru>
2023-11-14 13:17:52 +0300

    4. Thread без общих ресурсов [#267919]

---
+++ src/main/java/thread/sharedresources/UserCache.java
@@ -0,0 +1,25 @@
+package thread.sharedresources;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class UserCache {
+    private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
+    private final AtomicInteger id = new AtomicInteger();
+
+    public void add(User user) {
+        users.put(id.incrementAndGet(), User.of(user.getName()));
+    }
+
+    public User findById(int id) {
+        return User.of(users.get(id).getName());
+    }
+
+    public List<User> findAll() {
+        List<User> result = new ArrayList<>();
+        users.values().forEach(user -> result.add(User.of(user.getName())));
+        return result;
+    }
+}
